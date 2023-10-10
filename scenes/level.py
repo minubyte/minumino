7eca3dfdf2d7d5ce6ab40bf9314aedd231135acf
서민우 <alsdn638@gmail.com>
2023-10-10 20:59:49 +0900

    asdf

--- scenes/level.py
+++ scenes/level.py
@@ -3,6 +3,11 @@ from utils import *
 
 unit = 24
 
+das = 7
+arr = 1
+sdf = 1
+board_over = 4
+
 I = [
     [0, 0, 0, 0],
     [1, 1, 1, 1],
@@ -84,10 +89,11 @@ def moveable(mino, dx, dy, board):
             if dot == 1:
                 dot_x = x+dx
                 dot_y = y+dy
-                if dot_x < 0 or dot_x >= 10 or dot_y < -3 or dot_y >= 20:
-                    return
-                if board[dot_y][dot_x] == 1:
-                    return
+                if dot_x < 0 or dot_x >= 10 or dot_y >= 20:
+                    return False
+                elif dot_y > 0:
+                    if board[dot_y+board_over][dot_x] == 1:
+                        return False
     return True
 
 def rotateable(mino_t, mino, dx, dy, board, r, dr):
@@ -100,7 +106,7 @@ def rotateable(mino_t, mino, dx, dy, board, r, dr):
         offset_data = {[[0, 0]]}
     for offset in offset_data[key]:
         if moveable(rotated_mino, dx+offset[0], dy-offset[1], board):
-            return dx+offset[0], dy-offset[1], rotated_mino
+            return dx+offset[0], dy-offset[1]
 
 def rotate(mino, dr):
     if dr == 1:
@@ -110,6 +116,11 @@ def rotate(mino, dr):
     else:
         return rotate(rotate(mino, 1), 1)
 
+def lock(mino, dx, dy, board):
+    for y, row in enumerate(mino):
+        for x, dot in enumerate(row):
+            if dot == 1:
+                board[y+dy+board_over][x+dx] = 1
 
 class Level:
     def __init__(self, screen, gm):
@@ -118,14 +129,22 @@ class Level:
         self.cx = screen_width/2-unit*5 
         self.cy = screen_height/2-unit*10
 
-        self.mino_t = T
-        self.mino = self.mino_t
+        self.mino_t = T[:]
+        self.mino = self.mino_t[:]
         self.mino_x = 3
-        self.mino_ay = -3
-        self.mino_y = int(self.mino_ay)
+        self.mino_y_t = 0
+        self.mino_y = -board_over
         self.mino_r = 0
 
-        self.board = [[0 for _ in range(10)] for _ in range(20)]
+        self.das_t = 0
+        self.arr_t = 0
+        self.sdf_t = 0
+
+        self.lock_t = 0
+
+        self.dir = 0
+
+        self.board = [[0 for _ in range(10)] for _ in range(24)]
 
     def run(self, dt, events):
         self.screen.fill("#eeeeee")
@@ -133,24 +152,83 @@ class Level:
         for event in events:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    if moveable(self.mino, self.mino_x+1, self.mino_y, self.board):
-                        self.mino_x += 1
-                elif event.key == pygame.K_LEFT:
-                    if moveable(self.mino, self.mino_x-1, self.mino_y, self.board):
-                        self.mino_x -= 1
+                    self.dir = 1
+                else:
+                    self.dir = -1
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
+                    if moveable(self.mino, self.mino_x+self.dir, self.mino_y, self.board):
+                        self.mino_x += self.dir
+                    self.das_t = 0
+                    self.arr_t = 0
+                    self.lock_t = 0
                 elif event.key == pygame.K_UP:
                     rotated = rotateable(self.mino_t, self.mino, self.mino_x, self.mino_y, self.board, self.mino_r, 1)
-                    if rotated != False:
+                    if rotated:
                         self.mino_r = (self.mino_r+1)%4
-                        self.mino_x, self.mino_y, self.mino = rotated
-                    
-        self.mino_ay += dt/64
-        self.mino_y = int(self.mino_ay)
+                        self.mino_x, self.mino_y = rotated
+                        self.mino = rotate(self.mino, 1)
+                        self.lock_t = 0
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT] or keys[pygame.K_LEFT]:
+            if self.das_t < das:
+                self.das_t += dt
+            else:
+                if arr == 0:
+                    for i in range(0, 10):
+                        if moveable(self.mino, self.mino_x+self.dir, self.mino_y, self.board):
+                            self.mino_x += self.dir
+                        else:
+                            break
+                else:
+                    self.arr_t += dt
+                    if self.arr_t >= arr:
+                        self.arr_t = 0
+                        if moveable(self.mino, self.mino_x+self.dir, self.mino_y, self.board):
+                            self.mino_x += self.dir
+                            
+        if keys[pygame.K_DOWN]:
+            if sdf == 0:
+                for i in range(1, 23):
+                    if moveable(self.mino, self.mino_x, self.mino_y+1, self.board):
+                        self.mino_y += 1
+                    else:
+                        break
+            else:
+                self.sdf_t += dt
+                if self.sdf_t > sdf:
+                    self.sdf_t = 0
+                    if moveable(self.mino, self.mino_x, self.mino_y+1, self.board):
+                        self.mino_y += 1
+
+        self.mino_y_t += dt/64
+        if self.mino_y_t >= 1:
+            self.mino_y_t = 0
+            if moveable(self.mino, self.mino_x, self.mino_y+1, self.board):
+                self.mino_y += 1
+    
+        if not moveable(self.mino, self.mino_x, self.mino_y+1, self.board):
+            self.lock_t += dt
+            if self.lock_t >= 30:
+                lock(self.mino, self.mino_x, self.mino_y, self.board)
+                self.lock_t = 0
+                
+                self.mino_t = I[:]
+                self.mino = self.mino_t[:]
+                self.mino_x = 3
+                self.mino_y_t = 0
+                self.mino_y = -3
+                self.mino_r = 0
 
         for y, row in enumerate(self.mino):
             for x, dot in enumerate(row):
                 if dot == 1:
                     pygame.draw.rect(self.screen, "#000000", (self.cx+(self.mino_x+x)*unit, self.cy+(self.mino_y+y)*unit, unit, unit))
+                    
+        for y, row in enumerate(self.board):
+            for x, dot in enumerate(row):
+                if dot == 1:
+                    pygame.draw.rect(self.screen, "#000000", (self.cx+x*unit, self.cy+(y-board_over)*unit, unit, unit))
 
         for i in range(0, 20+1):
             pygame.draw.line(self.screen, "#000000", (self.cx, self.cy+i*unit), (self.cx+10*unit, self.cy+i*unit))
